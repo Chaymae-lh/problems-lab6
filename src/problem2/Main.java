865a0f765ad16e5df8bf08139879b6327a5350c3
Chaymae-lh <chaymaelahmama07@gmail.com>
2026-02-23 12:40:23 +0000

    Add files via upload

---
+++ src/problem2/Main.java
@@ -0,0 +1,20 @@
+package Problem_2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Bank bank = new Bank("My Bank");
+
+        bank.addCustomer("Salam");
+        bank.addCustomer("Ali");
+
+        bank.addCustomerTransaction("Salam", 500.0);   // credit
+        bank.addCustomerTransaction("Salam", -100.0);  // debit
+        bank.addCustomerTransaction("Ali", 300.0);
+
+        bank.printStatement("Salam");
+        System.out.println();
+        bank.printStatement("Ali");
+    }
+}
